4271a90c448b18bf4f2d056c78a7d327cda3ac3b
alexandercavin <alexander.cavin2@gmail.com>
2024-09-28 01:03:19 +0700

    push pertama asek

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Layout from './components/Layout'
+import Home from './pages/Home'
+import Detail from './pages/Detail';
+import Menu from './pages/Menu';
+import Checkout from './components/Checkout';
+import Success from './components/Success';
+import Login from './components/Login';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path='/menu' element={<Menu/>}/>
+          <Route path='/menu/:slug' element={<Detail />} />
+          <Route path='/checkout' element={<Checkout />} />
+          <Route path='/success' element={<Success />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
